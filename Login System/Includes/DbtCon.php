a0dbc4b1ae5bdf547b9fe7d7ea58cbb4044e5a54
TheSharkJO <Johnnyorellana32@gmail.com>
2020-01-10 12:16:50 -0500

    Added Necessary Files

---
+++ Login System/Includes/DbtCon.php
@@ -0,0 +1,22 @@
+<?php
+
+  $host = "localhost";
+  $port = "5432";
+  $dbname = "myDatabase"
+  $user = "postgres"
+  $password = "newDatabase21";
+
+  $dbconn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass")
+
+  $stat = pg_connection_status($dbconn);
+  if ($stat === PGSQL_CONNECTION_OK){
+  echo 'liberty';
+}else {
+echo "connection failed";
+}
+
+if (!$dbconn){
+die("Connection Failed: ");
+}
+
+?>
--- Login System/LoginPage.php
+++ Login System/LoginPage.php
@@ -1,1 +1,14 @@
+<?php
 
+
+// Initialize Login-Session
+session_start();
+
+//Check to see if user is logged in, if so redirects the user to welcome page
+if(isset($ SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: welcome.php");
+  exit;
+}
+
+
+ ?>
