d32a0357d08328e31a05e6553933c92a893bd8a0
JohnnytheShark <orellanajonathan7@gmail.com>
2020-01-12 19:49:36 -0500

    Minor Changes to Syntax

--- Login System/Includes/DbtCon.php
+++ Login System/Includes/DbtCon.php
@@ -1,12 +1,12 @@
 <?php
 
   $host = "localhost";
-  $port = "5432";
-  $dbname = "myDatabase"
-  $user = "postgres"
+  $port = 5432;
+  $dbname = "myDatabase";
+  $user = "postgres";
   $password = "newDatabase21";
 
-  $dbconn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass")
+  $dbconn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
 
   $stat = pg_connection_status($dbconn);
   if ($stat === PGSQL_CONNECTION_OK){
